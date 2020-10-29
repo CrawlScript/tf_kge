a9fc9eb4b911a31a09e18fc7f14026ba031c109a
hujunxianligong <hujunxianligong@gmail.com>
2020-10-29 09:32:31 +0800

    colab

--- test.py
+++ test.py
@@ -12,11 +12,17 @@ import numpy as np
 train_kg, test_kg, valid_kg, entity_indexer, relation_indexer = WN18Dataset().load_data()
 
 embedding_size = 20
+margin = 2.0
+train_batch_size = 5000
+test_batch_size = 100
+
+
 entity_embeddings = tf.Variable(tf.random.truncated_normal([train_kg.num_entities, embedding_size], stddev=np.sqrt(1 / embedding_size)))
 relation_embeddings = tf.Variable(tf.random.truncated_normal([train_kg.num_relations, embedding_size], stddev=np.sqrt(1 / embedding_size)))
 
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
+
 
 def compute_distance(a, b):
     # return tf.reduce_sum((a - b) ** 2, axis=-1)
@@ -25,45 +31,45 @@ def compute_distance(a, b):
 
 for epoch in range(10000):
 
-    for step, (batch_h, batch_r, batch_t) in tqdm(enumerate(tf.data.Dataset.from_tensor_slices((train_kg.h, train_kg.r, train_kg.t)).shuffle(10000).batch(1000))):
-        for target_entity_type in ["head", "tail"]:
-            with tf.GradientTape() as tape:
-                if target_entity_type == "tail":
-                    batch_source = batch_h
-                    batch_target = batch_t
-                else:
-                    batch_source = batch_t
-                    batch_target = batch_h
+    for step, (batch_h, batch_r, batch_t) in enumerate(tf.data.Dataset.from_tensor_slices((train_kg.h, train_kg.r, train_kg.t)).shuffle(10000).batch(train_batch_size)):
+        target_entity_type = "head" if np.random.randint(0, 2) == 0 else "tail"
+        # for target_entity_type in ["head", "tail"]:
+        with tf.GradientTape() as tape:
+            if target_entity_type == "tail":
+                batch_source = batch_h
+                batch_target = batch_t
+            else:
+                batch_source = batch_t
+                batch_target = batch_h
+
+            batch_neg_target = entity_negative_sampling(batch_source, batch_r, kg=train_kg, target_entity_type=target_entity_type, filtered=True)
+
+            embedded_source = tf.nn.embedding_lookup(entity_embeddings, batch_source)
+            embedded_r = tf.nn.embedding_lookup(relation_embeddings, batch_r)
+            embedded_target = tf.nn.embedding_lookup(entity_embeddings, batch_target)
+            embedded_neg_target = tf.nn.embedding_lookup(entity_embeddings, batch_neg_target)
+
+            if target_entity_type == "tail":
+                translated = embedded_source + embedded_r
+            else:
+                translated = embedded_source - embedded_r
 
 
-                batch_neg_target = entity_negative_sampling(batch_source, batch_r, kg=train_kg, target_entity_type=target_entity_type, filtered=True)
+            losses = tf.maximum(
+                margin + compute_distance(translated, embedded_target) - compute_distance(translated, embedded_neg_target), 0.0)
 
-                embedded_source = tf.nn.embedding_lookup(entity_embeddings, batch_source)
-                embedded_r = tf.nn.embedding_lookup(relation_embeddings, batch_r)
-                embedded_target = tf.nn.embedding_lookup(entity_embeddings, batch_target)
-                embedded_neg_target = tf.nn.embedding_lookup(entity_embeddings, batch_neg_target)
+            loss = tf.reduce_mean(losses)
 
-                if target_entity_type == "tail":
-                    translated = embedded_source + embedded_r
-                else:
-                    translated = embedded_source - embedded_r
+        vars = tape.watched_variables()
+        grads = tape.gradient(loss, vars)
+        optimizer.apply_gradients(zip(grads, vars))
 
-                margin = 2.0
-                losses = tf.maximum(
-                    margin + compute_distance(translated, embedded_target) - compute_distance(translated, embedded_neg_target), 0.0)
-
-                loss = tf.reduce_mean(losses)
-
-            vars = tape.watched_variables()
-            grads = tape.gradient(loss, vars)
-            optimizer.apply_gradients(zip(grads, vars))
-
-            if step % 200 == 0:
-                print("epoch = {}\tstep = {}\tloss = {}".format(epoch, step, loss))
+        if step % 200 == 0:
+            print("epoch = {}\tstep = {}\tloss = {}".format(epoch, step, loss))
 
 
     mean_ranks = []
-    for test_step, (batch_h, batch_r, batch_t) in tqdm(enumerate(tf.data.Dataset.from_tensor_slices((test_kg.h, test_kg.r, test_kg.t)).batch(20))):
+    for test_step, (batch_h, batch_r, batch_t) in enumerate(tf.data.Dataset.from_tensor_slices((test_kg.h, test_kg.r, test_kg.t)).batch(test_batch_size)):
         batch_source = batch_h
         batch_target = batch_t
 
